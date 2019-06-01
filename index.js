e4ff34829e98b1a1076f809419b3753bf29e0b72
EricB <ericblancas23@gmail.com>
2019-06-01 00:28:12 -0700

    sounds working

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll('.sound');
-    const pads = document.querySelectorAll('.pads');
-})
+    const pads = document.querySelectorAll('.pads div');
+
+    console.log(sounds);
+    //sounds functionality
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function(){
+            sounds[index].play()
+        });
+    });
+});
